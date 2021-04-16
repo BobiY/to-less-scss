d134bf7c86841b8590c097c62840ee87fd8ef4b0
@yaoshuaibo <769726708@qq.com>
2021-04-16 22:58:54 +0800

    fix: 修改less 文件类型

--- toLess/index.js
+++ toLess/index.js
@@ -4,13 +4,11 @@ import fs from "fs";
 import path from "path";
 import parseScope from "./varParse/index";
 import printOut from "../utils/printing";
-const text = fs.readFileSync(path.join(__dirname, '../', '/ycss/varText.ycss'))
+const text = fs.readFileSync(path.join(__dirname, '../', '/ycss/varText.less'))
 
 const node = postcss.parse(text)
 
 parseScope(node)
 
 printOut(node.toResult().css)//
-
-
 
